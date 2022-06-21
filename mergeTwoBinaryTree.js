7a35e922f26b791cb825425724d3da4bdf388872
go <xxxviii00@gmail.com>
2022-06-22 00:23:19 +0900

    [LeetCode] 617. Merge Two Binary Trees

---
+++ mergeTwoBinaryTree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 617. Merge Two Binary Trees
+ * @see https://leetcode.com/problems/merge-two-binary-trees/
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+var mergeTrees = function (root1, root2) {
+  if (root1 && root2) {
+    const node = new TreeNode(root1.val + root2.val);
+    node.left = mergeTrees(root1.left, root2.left);
+    node.right = mergeTrees(root1.right, root2.right);
+    return node;
+  }
+
+  return root1 || root2;
+};
