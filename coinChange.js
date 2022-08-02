d6452a2e088c423127d515cdd7015822a3c015dc
go <xxxviii00@gmail.com>
2022-08-02 23:17:58 +0900

    [LeetCode]322. Coin Change

---
+++ coinChange.js
@@ -0,0 +1,19 @@
+/**
+ * 322. Coin Change
+ * @see https://leetcode.com/problems/coin-change/
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  let dp = Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+
+  for (let coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+    }
+  }
+
+  return dp[amount] < Infinity ? dp[amount] : -1;
+};
