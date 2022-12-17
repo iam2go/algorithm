111581a1c0f8a84dc309d0b6a4ed8ebd1631c5f5
go <xxxviii00@gmail.com>
2022-12-18 00:36:05 +0900

    [Programmers] 땅따먹기

---
+++ hopscotch.js
@@ -0,0 +1,21 @@
+/**
+ * 땅따먹기
+ * @see https://school.programmers.co.kr/learn/courses/30/lessons/12913
+ * @param {number[][]} land 
+ * @returns {number}
+ */
+function solution(land) {
+  let result = land[0].slice();
+
+  const getMaxValue = (except, values) => {
+    const _values = values.slice();
+    _values.splice(except, 1);
+    return Math.max(..._values);
+  };
+
+  for (let row = 1, len = land.length; row < len; row++) {
+    result = result.map((_, index) =>  land[row][index] + getMaxValue(index, result)
+  }
+
+  return Math.max(...result);
+}
